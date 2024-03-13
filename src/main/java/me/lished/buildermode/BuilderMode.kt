08dd27184df0cc05c38d1d28da7e651d7fb37403
BuildTools <skywarspanel@gmail.com>
2024-03-13 14:33:28 +0100

    Add Kotlin lib to plugin.yml

--- src/main/java/me/lished/buildermode/BuilderMode.kt
+++ src/main/java/me/lished/buildermode/BuilderMode.kt
@@ -1,17 +1,13 @@
-package me.lished.buildermode;
+package me.lished.buildermode
 
-import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.plugin.java.JavaPlugin
 
-public final class BuilderMode extends JavaPlugin {
-
-    @Override
-    public void onEnable() {
+class BuilderMode : JavaPlugin() {
+    override fun onEnable() {
         // Plugin startup logic
-
     }
 
-    @Override
-    public void onDisable() {
+    override fun onDisable() {
         // Plugin shutdown logic
     }
 }
