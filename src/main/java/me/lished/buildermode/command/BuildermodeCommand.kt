0de68d867f6eed698077ad6df1fe6f72e5167fa3
BuildTools <skywarspanel@gmail.com>
2024-03-13 16:30:09 +0100

    Add /builermode logics

--- src/main/java/me/lished/buildermode/command/BuildermodeCommand.kt
+++ src/main/java/me/lished/buildermode/command/BuildermodeCommand.kt
@@ -1,12 +1,21 @@
 package me.lished.buildermode.command
 
+import me.lished.buildermode.manager.BuildermodeManager
 import org.bukkit.command.Command
 import org.bukkit.command.CommandExecutor
 import org.bukkit.command.CommandSender
+import org.bukkit.entity.Player
 
 class BuildermodeCommand : CommandExecutor {
     override fun onCommand(sender: CommandSender, command: Command, label: String, args: Array<out String>?): Boolean {
-        TODO("Not yet implemented")
+        val player = sender as Player
+        if (BuildermodeManager.builders.contains(player)) {
+            BuildermodeManager.builders.remove(player)
+            player.sendMessage("Builder mode disabled.")
+        } else {
+            BuildermodeManager.builders.add(player)
+            player.sendMessage("Builder mode enabled.")
+        }
     }
 
 }
---
+++ src/main/java/me/lished/buildermode/manager/BuildermodeManager.kt
@@ -0,0 +1,8 @@
+package me.lished.buildermode.manager
+
+import org.bukkit.entity.Player
+
+object BuildermodeManager {
+    var builders = mutableSetOf<Player>()
+
+}
