60738408ead52e9428f2ebd6c6018b6a896589ad
BuildTools <skywarspanel@gmail.com>
2024-03-13 14:51:30 +0100

    Add /builermode command logics file

---
+++ src/main/java/me/lished/buildermode/command/BuildermodeCommand.kt
@@ -0,0 +1,12 @@
+package me.lished.buildermode.command
+
+import org.bukkit.command.Command
+import org.bukkit.command.CommandExecutor
+import org.bukkit.command.CommandSender
+
+class BuildermodeCommand : CommandExecutor {
+    override fun onCommand(sender: CommandSender, command: Command, label: String, args: Array<out String>?): Boolean {
+        TODO("Not yet implemented")
+    }
+
+}
