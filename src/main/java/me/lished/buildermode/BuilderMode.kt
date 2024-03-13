d10e6738be220d2631bf7a3081fc632919ab7feb
BuildTools <skywarspanel@gmail.com>
2024-03-13 14:37:53 +0100

    Add /builermode instead

--- src/main/java/me/lished/buildermode/BuilderMode.kt
+++ src/main/java/me/lished/buildermode/BuilderMode.kt
@@ -4,10 +4,6 @@ import org.bukkit.plugin.java.JavaPlugin
 
 class BuilderMode : JavaPlugin() {
     override fun onEnable() {
-        // Plugin startup logic
-    }
-
-    override fun onDisable() {
-        // Plugin shutdown logic
+        getCommand("buildermode").setExecutor(Build)
     }
 }
