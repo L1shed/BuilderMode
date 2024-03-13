6bb801f021427c712213811ec819245cdd9dde88
BuildTools <skywarspanel@gmail.com>
2024-03-13 16:53:30 +0100

    Remove bad imports

--- src/main/java/me/lished/buildermode/BuilderMode.kt
+++ src/main/java/me/lished/buildermode/BuilderMode.kt
@@ -1,10 +1,12 @@
 package me.lished.buildermode
 
 import me.lished.buildermode.command.BuildermodeCommand
+import me.lished.buildermode.listener.BuildermodeListener
 import org.bukkit.plugin.java.JavaPlugin
 
 class BuilderMode : JavaPlugin() {
     override fun onEnable() {
         getCommand("buildermode")!!.setExecutor(BuildermodeCommand())
+        server.pluginManager.registerEvents(BuildermodeListener(this), this)
     }
 }
--- src/main/java/me/lished/buildermode/listener/BuildermodeListener.kt
+++ src/main/java/me/lished/buildermode/listener/BuildermodeListener.kt
@@ -1,4 +1,40 @@
 package me.lished.buildermode.listener
 
-class BuildermodeListener {
+import org.bukkit.Bukkit
+import org.bukkit.block.Block
+import org.bukkit.entity.Player
+import org.bukkit.event.EventHandler
+import org.bukkit.event.Listener
+import org.bukkit.event.block.BlockBreakEvent
+import org.bukkit.event.block.BlockPlaceEvent
+import org.bukkit.metadata.FixedMetadataValue
+import org.bukkit.metadata.MetadataValue
+import org.bukkit.plugin.java.JavaPlugin
+
+class BuildermodeListener(private val plugin: JavaPlugin): Listener {
+    @EventHandler
+    fun onPlace(e: BlockPlaceEvent) {
+        val block: Block = e.blockPlaced
+        block.setMetadata("placed_by", FixedMetadataValue(plugin, e.player.name))
+    }
+
+    @EventHandler
+    fun onBlockBreak(event: BlockBreakEvent) {
+        val player: Player = event.player
+        val block: Block = event.block
+
+        // Retrieve metadata
+        val metadataList: List<MetadataValue> = block.getMetadata("placedBy")
+
+        // Check if metadata exists
+        if (metadataList.isNotEmpty()) {
+            val placedBy: String = metadataList[0].asString()
+            if (Bukkit.getPlayer(placedBy) != player) {
+                event.isCancelled = true
+            }
+            player.sendMessage("This block was placed by: $placedBy")
+        } else {
+            player.sendMessage("no data found")
+        }
+    }
 }
